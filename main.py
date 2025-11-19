8eb0416e5ad211536d62f46d5b7856bc5385ff97
gorgick <gorgick2014@yandex.by>
2025-11-19 16:20:20 +0300

    proof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import hashlib
 
 
 class Blockchain:
@@ -16,3 +17,17 @@ class Blockchain:
 
     def print_previous_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while check_proof is False:
+            hash_operation = hashlib.sha256(
+                str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:5] == '00000':
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof
